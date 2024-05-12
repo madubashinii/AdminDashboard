5eed15c8b93693eaa02c012a3522f7313594b494
Nilakshi Madubashini <167696066+madubashinii@users.noreply.github.com>
2024-05-12 17:43:21 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,3 @@
 <?php 
-    //create connection
-    $conn = new mysqli("localhost","root","","wildwaves",3307);
-
-    //check connection
-    if($conn->connect_error){
-        die("Data base connection error : ". $conn->connect_error);
-    }
+    
 ?>
